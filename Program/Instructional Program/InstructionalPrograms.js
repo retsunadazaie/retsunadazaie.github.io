6f667bdc6b149ba35c3747d7484c6edb083839f4
Jensuu <nolongerhumanlean@gmail.com>
2024-04-25 10:13:37 +0800

    2

--- Program/Instructional Program/InstructionalPrograms.js
+++ Program/Instructional Program/InstructionalPrograms.js
@@ -55,7 +55,7 @@ document.getElementById("readmore_button").onclick = function() {
 
   // Scroll to the position of the heading element smoothly
   window.scrollTo({
-      top: 595,
+      top: 610,
       behavior: 'smooth'
   });
 };
--- Program/Special Programs/specialprograms.js
+++ Program/Special Programs/specialprograms.js
@@ -57,7 +57,7 @@ document.getElementById("readmore_button").onclick = function() {
 
   // Scroll to the position of the heading element smoothly
   window.scrollTo({
-      top: 595,
+      top: 610,
       behavior: 'smooth'
   });
 };
