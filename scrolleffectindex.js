97e0def4c842d5be8215aeb92d8bbecd25b3d409
Jensuu <nolongerhumanlean@gmail.com>
2024-04-25 15:54:55 +0800

    1

--- scrolleffectindex.js
+++ scrolleffectindex.js
@@ -1,14 +1,14 @@
 const letters = document.querySelectorAll('.letter');
-const meaningDisplay = document.getElementById('meaning');
+  const meaningDisplay = document.getElementById('meaning');
 
-letters.forEach(letter => {
-  letter.addEventListener('mouseover', () => {
-    const meaning = letter.getAttribute('data-meaning');
-    meaningDisplay.textContent = meaning;
-    meaningDisplay.classList.add('show');
-  });
+  letters.forEach(letter => {
+    letter.addEventListener('mouseover', () => {
+      const meaning = letter.getAttribute('data-meaning');
+      meaningDisplay.textContent = meaning;
+      meaningDisplay.classList.add('show');
+    });
 
-  letter.addEventListener('mouseout', () => {
-    meaningDisplay.classList.remove('show');
+    letter.addEventListener('mouseout', () => {
+      meaningDisplay.classList.remove('show');
+    });
   });
-});
